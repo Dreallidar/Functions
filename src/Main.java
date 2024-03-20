3d09cfcd4676319b23a142c513987725660be5fd
Dreallidar <Realdreanix@gmail.com>
2024-03-20 09:02:18 +0200

    Up to Easy task 5 complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,57 @@
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("~~~~Task 1~~~~");
+        result(4, 12);
+        result(6, 8);
+        //  System.out.println(9.8596);
+        System.out.println("~~~~ Task 2~~~~~");
+        System.out.println(PISq());
+        double resDobel = PISq();
+        System.out.println(resDobel);
+        System.out.println("~~~Task 3~~~~~~~");
+        System.out.println(daugyba(15, 16));
+        System.out.println("~~~~~~  Task 4~~~~~~");
+        int[] arr = {1, 5, 9, 6, 3, 5, 2};
+        printArr(arr);
+        System.out.println("~~~~~~~ Task 5~~~~~~~~~");
+        System.out.println(rngInt(50,300));
+
     }
+
+    public static void result(int a, int b) {
+
+        System.out.println(a + b);
+
+    }
+
+    public static double PISq() {
+        return 9.8596;
+    }
+
+
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+
+    }
+
+    public static int daugyba(int a, int b) {
+
+        return a * b;
+    }
+
+
+
+
+public static int rngInt(int min, int max) {
+    int generate = (int) (min + (Math.random() * (max - min)));
+    return generate;
+}
+
+
 }
